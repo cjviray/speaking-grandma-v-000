737c6e90ef7858df4ff4c8abfa205163c8976210
Christian V <viray.christianj@gmail.com>
2018-09-14 18:29:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  else if phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
-  else if phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  end
-end 
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+   end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
